59412abf903a705ff1f420fce6e43422610dcd8b
Radhika Suri <Radhika.Suri@smu.ca>
2021-11-28 22:36:16 -0400

    Initial commit for the project solution

---
+++ Team12_FlightReservationSystem/FlightReservationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlightReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
